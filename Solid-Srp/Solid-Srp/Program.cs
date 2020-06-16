b2aff540409b008413e81e64a8ddcff9cb5ac50b
burhan70 <brhk70@gmail.com>
2020-06-16 05:44:22 +0300

    Add WorkReport, WorkReportEntry

--- Solid-Srp/Solid-Srp/Program.cs
+++ Solid-Srp/Solid-Srp/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Solid_Srp
+﻿namespace Solid_Srp
 {
+
     class Program
     {
         static void Main(string[] args)
         {
         }
     }
+
+    
 }
---
+++ Solid-Srp/Solid-Srp/WorkReportEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Solid_Srp
+{
+    public class WorkReportEntry
+    {
+        public string ProjectCode { get; set; }
+        public string ProjectName { get; set; }
+        public int SpentHours { get; set; }
+    }
+}
