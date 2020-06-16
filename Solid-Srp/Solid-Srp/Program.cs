093ccdfbefd3913d1e3a1f82e5a86ca45fadccc3
burhan70 <brhk70@gmail.com>
2020-06-16 05:54:39 +0300

    Add code that use WorkReport

--- Solid-Srp/Solid-Srp/Program.cs
+++ Solid-Srp/Solid-Srp/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Solid_Srp
+﻿using System;
+
+namespace Solid_Srp
 {
 
     class Program
     {
         static void Main(string[] args)
         {
+            var wr = new WorkReport();
+
+            wr.AddEntry(new WorkReportEntry
+            {
+                ProjectCode = "IOT",
+                ProjectName = "Internet Of Things",
+                SpentHours = 3
+            });
+            wr.AddEntry(new WorkReportEntry
+            {
+                ProjectCode = "AI",
+                ProjectName = "Artificial Intelegence",
+                SpentHours = 5
+            });
+
+            wr.SaveToFile(@"C:\Temp\", "WorkHours.txt");
+
+            Console.WriteLine(wr);
+
+            Console.ReadKey();
         }
     }
 
